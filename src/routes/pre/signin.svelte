8c7b05f56e04f7818391d9d4a46344990fe6d79c
walikhan10 <walikhan10@gmail.com>
2021-10-17 17:57:05 -0400

    api working

--- src/routes/pre/signin.svelte
+++ src/routes/pre/signin.svelte
@@ -2,27 +2,33 @@
     let email;
     let password;
 
-    // const submitForm = async () =>{
-    //     try {
-    //         const submit = await fetch("https://strengthn.herokuapp.com/auth/login/", {
-    //             method: "POST",
-    //             headers: {
-    //                 'Content-Type': 'application/json'
-    //             },
-    //             body: JSON.stringify({
-    //                 email,
-    //                 password
-    //             }),
-    //         }); 
-    //         const data = await submit.json();
-    //         console.log(data);
-    //     } catch (err){
-    //         console.log(err)
-    //     } 
-    // };
+    const submitForm = async () =>{
+        var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "email": email,
+  "password": password
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://strengthn.herokuapp.com/auth/login/", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+    };
 
 </script>
 <h1>Welcome Back</h1>
+<body>
+<form on:submit|preventDefault = {submitForm}>
+<div class="container">
 <!-- <form on:submit|preventDefault = {submitForm}> -->
 <input type = "text" name = "email" placeholder = "Email" bind:value={email}>
 <input type = "text" name = "password" placeholder = "Password" bind:value={password}>
@@ -31,6 +37,9 @@
     <input type="submit" value = "Create Account"/>  
 </a>
 
+</div>
+</form>
+</body>
 <!-- </form> -->
 
 <style>
