6de3890f51534e5b94bbd538826709efc768a547
walikhan10 <walikhan10@gmail.com>
2021-10-17 23:23:27 -0400

    changes

--- src/routes/dash/messages.svelte
+++ src/routes/dash/messages.svelte
@@ -3,3 +3,30 @@
 </svelte:head>
 
 <h1>Messages</h1>
+
+
+
+<script>
+
+var myHeaders = new Headers();
+myHeaders.append("token", "\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiMDIxYzdhMjktNjMxMi00YThlLTk4OTQtZGEyZTc3NThiYWZmIn0sImlhdCI6MTYzNDUyMzc2OSwiZXhwIjoxNjM0NTI3MzY5fQ.C94-82dKLmICGrN1fOj5-y5Vlg_iVPFkuu1avAfj_yw\"");
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "message": "yooo"
+});
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://strengthn.herokuapp.com/user/messages/4", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+      
+    
+    </script>
