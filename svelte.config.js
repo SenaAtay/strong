a35da6c21752ba08b2016a8f07c6a61737436d84
Sena Atay <senatay@live.unc.edu>
2021-10-18 00:07:32 -0400

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node({ env: { port: process.env.PORT } }),
+		adapter: node({ env: { port: process.env.PORT } })
 
 	},
 };
