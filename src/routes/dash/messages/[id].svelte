1ef6df241ea50bed5a5f27f96a86ea299390f078
Sena Atay <senatay@live.unc.edu>
2021-11-01 07:35:04 -0400

    post

--- src/routes/dash/messages/[id].svelte
+++ src/routes/dash/messages/[id].svelte
@@ -1,5 +1,6 @@
 <script context="module">
     import {jwt} from "../../../stores/jwt";
+
     export async function load({page}) {
         // console.log("page.params.id", page.params.id);
         const id = page.params.id;
@@ -7,24 +8,63 @@
         const unsubscribe = jwt.subscribe(value => {
             njwt = value;
         })
-        const res = await fetch (`https://strengthn.herokuapp.com/user/messages/${id}`, {
+        
+            const res = await fetch (`https://strengthn.herokuapp.com/user/messages/${id}`, {
             method: "GET",
             headers:{
                 'Content-Type': 'application/json',
                 "token": JSON.stringify(njwt),
             },
         });
-
         const groupmessages = await res.json();
         // console.log("groupmessages", groupmessages);
+
+        
         return {props: {groupmessages}};
+          
+
     }
+        setInterval(load, 1000);
+
 </script>
 
 <script>
+    import {page} from '$app/stores'
     export let groupmessages;
     $: meh = groupmessages;
-    // console.log(groupmessages[0].groupid);
+    let message;
+    
+    const submit = async () =>{
+
+        const id = $page.params.id;
+        let njwt;
+        const unsubscribe = jwt.subscribe(value => {
+            njwt = value;
+        })
+        try {
+            const submit = await fetch(`https://strengthn.herokuapp.com/user/messages/${id}`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    "token": JSON.stringify(njwt),
+                },
+                body: JSON.stringify({
+                    message
+                }),
+            }); 
+            
+            const predata = await submit;
+            const data = await submit.json();
+            console.log("consoling data", data);
+   
+          
+        } catch (err){
+            console.log(err)
+        } 
+        
+    };
+
+    
     
 </script>
 
@@ -34,4 +74,12 @@
 <p>{message}</p>
 {/each} 
 
+<form on:submit|preventDefault={submit}>
+    <input placeholder="Write a message" bind:value={message}>
+</form>
 
+<!-- const fetchMessage = async() => {
+
+if (njwt) {
+    setInterval(fetchMessage, 7000);
+}  -->
